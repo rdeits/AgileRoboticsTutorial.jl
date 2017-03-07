73ff1c64ed072d37e23bd10298f474abf8605566
Robin Deits <robin.deits@gmail.com>
2017-03-07 13:51:48 -0500

    change test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,8 @@ C_code = """
 #include <stddef.h>
 double c_sum(size_t n, double *X) {
     double s = 0.0;
-    for (size_t i = 0; i < n; ++i) {
+    size_t i;
+    for (i = 0; i < n; ++i) {
         s += X[i];
     }
     return s;
