cc5d62a9199fd0a30430e480da0d1d1653056192
Robin Deits <robin.deits@gmail.com>
2017-09-21 11:43:10 -0400

    go back to running notebooks separately

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,19 @@
 using AgileRoboticsTutorial
 using Base.Test
-using NBInclude
-using Reactive
 
 const module_tempdir = joinpath(Base.tempdir(), string(module_name(AgileRoboticsTutorial)))
 
 @testset "example notebooks" begin
+    using IJulia
+
+    outputdir = module_tempdir
+    if !isdir(outputdir)
+        mkpath(outputdir)
+    end
+    jupyter = IJulia.jupyter
     for f in filter(x -> endswith(x, "ipynb"), readdir("../notebooks"))
         notebook = joinpath("..", "notebooks", f)
-        cd(dirname(notebook)) do
-            nbinclude(notebook)
-        end
+        output = joinpath(outputdir, f)
+        @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output --ExecutePreprocessor.timeout=500`); true end
     end
 end
