7c42ace5a140127795df1115566f0b9ebed7dcb5
Robin Deits <robin.deits@gmail.com>
2017-09-21 01:12:48 -0400

    test with nbinclude to avoid kernel timeout issues

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,12 @@
 using AgileRoboticsTutorial
 using Base.Test
+using NBInclude
 
 const module_tempdir = joinpath(Base.tempdir(), string(module_name(AgileRoboticsTutorial)))
 
 @testset "example notebooks" begin
-    using IJulia
-
-    outputdir = module_tempdir
-    if !isdir(outputdir)
-        mkpath(outputdir)
-    end
-    jupyter = IJulia.jupyter
     for f in filter(x -> endswith(x, "ipynb"), readdir("../notebooks"))
         notebook = joinpath("..", "notebooks", f)
-        output = joinpath(outputdir, f)
-        @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output --ExecutePreprocessor.timeout=90`); true end
+        nbinclude(notebook)
     end
 end
