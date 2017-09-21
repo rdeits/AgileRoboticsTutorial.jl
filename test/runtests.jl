436903fc570b13befb0ccc22f0aaab97461f050a
Robin Deits <robin.deits@gmail.com>
2017-09-21 11:22:15 -0400

    try to fix nbinclude tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,15 @@
 using AgileRoboticsTutorial
 using Base.Test
 using NBInclude
+using Reactive
 
 const module_tempdir = joinpath(Base.tempdir(), string(module_name(AgileRoboticsTutorial)))
 
 @testset "example notebooks" begin
     for f in filter(x -> endswith(x, "ipynb"), readdir("../notebooks"))
         notebook = joinpath("..", "notebooks", f)
-        nbinclude(notebook)
+        cd(dirname(notebook)) do
+            nbinclude(notebook)
+        end
     end
 end
